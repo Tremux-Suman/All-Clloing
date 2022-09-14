e0af22f5d8b851c52f56972306c26a2a24e9e75e
Md Suman Hossain <111163810+Tremux-Suman@users.noreply.github.com>
2022-09-14 10:40:51 +0600

    Update SUMAN.py

--- SUMAN.py
+++ SUMAN.py
@@ -3,9 +3,9 @@ os.system('clear')
 print('[•] Checking Updates...')
 os.system('git pull')
 bit = platform.architecture()[0]
-if bit=='64bit':
+if bit=='32bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
-    os.system('xdg-open https://facebook.com/groups/351076900316263/')
-    import AKING
+    os.system('xdg-open https://www.facebook.com/groups/faithar.killer.hacker.tm/')
+    import SUMAN_32
 else:
     exit('\033[1;31m[×] Sorry Device Not Support')
