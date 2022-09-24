b5bebffee1607e201cc785abd57a58035c8ef220
Md Suman Hossain <111163810+Tremux-Suman@users.noreply.github.com>
2022-09-24 16:53:58 +0600

    Update SUMAN.py

--- SUMAN.py
+++ SUMAN.py
@@ -15,6 +15,6 @@ if bit=='64bit':
 elif bit=='32bit':
     print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
     os.system('xdg-open https://www.facebook.com/groups/faithar.killer.hacker.tm/')
-    import 
+    import SUMAN_32
 else:
     print(f'{red}[×] Sorry System Not Support{white}')
