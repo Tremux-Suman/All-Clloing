3866cbbc7460202bd6f620db1247526a0d6f712f
Md Suman Hossain <111163810+Tremux-Suman@users.noreply.github.com>
2022-09-27 06:50:48 +0600

    Update SUMAN.py

--- SUMAN.py
+++ SUMAN.py
@@ -1,11 +1,20 @@
-import os,platform
+import os,time,platform
 os.system('clear')
 print('[•] Checking Updates...')
 os.system('git pull')
+green = ('\033[1;32m')
+white = ('\033[1;37m')
+red = ('\033[1;31m')
+
+print('<------------------------------------>')
 bit = platform.architecture()[0]
 if bit=='64bit':
-    print('\033[1;32m[•] Congrats! Your Device Support This Tools \033[1;37m')
+    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
     os.system('xdg-open https://facebook.com/groups/351076900316263/')
     import SUM
+elif bit=='32bit':
+    print(f'{green}[•] Join Over Facebook Group For Any Help{white}')
+    os.system('xdg-open https://facebook.com/groups/351076900316263/')
+    import SUMAN_32
 else:
-    exit('\033[1;31m[×] Sorry Device Not Support')
+    print(f'{red}[×] Sorry System Not Support{white}')
